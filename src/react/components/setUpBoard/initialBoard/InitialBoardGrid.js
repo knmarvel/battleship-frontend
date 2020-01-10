a946fbc8f3a85a6aefd8765ff35844e83625d797
Karen Marvel <knmarvel@gmail.com>
2020-01-10 12:15:19 -0500

    the branch says it all

--- src/react/components/setUpBoard/initialBoard/InitialBoardGrid.js
+++ src/react/components/setUpBoard/initialBoard/InitialBoardGrid.js
@@ -1,22 +1,17 @@
 import React from "react";
 import InitialBoardSquare from "./InitialBoardSquare";
 import { connect } from "../../../HOCs";
-import { fetchLastMessage } from "../../../../redux/index";
+import { placeShip } from "../../../../redux/index";
 
 class InitialBoardGrid extends React.Component {
-  state = {
-    playerName: this.props.playerName
-  };
 
   label = "";
   newRow = [];
   newBoard = [];
   //use nested loops to define the initial divs
   rowLabels = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+ 
 
-  componentDidMount = () => {
-    console.log(this.state.playerName);
-  };
 
   drawSquare = label => {
     return (
@@ -41,36 +36,14 @@ class InitialBoardGrid extends React.Component {
     console.log("target row is " + targetRow);
     console.log("target column is " + targetColumn);
     console.log(e.target.innerHTML);
-    this.readShipName();
+    // console.log(this.props.playerName)
+    // if(this.props.activeShip === null){
+    //   console.log(null)
+    // }
+    // else{console.log(this.props.activeShip.name)}
   };
 
-  readShipName = () => {
-    //====================================================
-    //janell says:  kano - i'm using connect to use a function "fetchLastMessage"
-    //with redux that should return the last message that the player posted.
-    //it's returning a promise.  i want the answer lol!
-    //the last word of the answer should be the name of the ship,
-    //which can be used for the calculations.
-    //i think the solution lies in the promise and await features but
-    //that's where i got stuck.
-    //kano says: we need to open the message object and then get the last word
-    //=====================================================
-    //
-
-    fetch(
-      `https://battleship-capstone-api.herokuapp.com/messages?limit=1&offset=0&username=${this.state.playerName}`
-    )
-      .then(responseObject => responseObject.json())
-      .then(res => {
-        let lastMessage = res.messages[0];
-        console.log(lastMessage);
-        // let lastWord = this.determineLastWordOfLastMessage(lastMessage);
-        // console.log(lastWord);
-      });
-  };
-
-  determineLastWordOfLastMessage = message => {};
-
+  
   render() {
     //first, draw the header row
     for (let headerRowLabels = 0; headerRowLabels <= 10; headerRowLabels++) {
@@ -104,14 +77,14 @@ class InitialBoardGrid extends React.Component {
   }
 }
 
-//===================
-//stopping point: shaquon meet 11 am zoom tomorrow
-//====================
 
 const mapStateToProps = state => {
-  return { playerName: state.auth.login.result.username };
+  return { 
+      playerName: state.auth.login.result.username,
+      activeShip: state.setUpGame.selectShip.result ? state.setUpGame.selectShip.result : null 
+    };
 };
 
-const mapDispatchToProps = { fetchLastMessage };
+const mapDispatchToProps = { placeShip };
 
 export default connect(mapStateToProps, mapDispatchToProps)(InitialBoardGrid);
--- src/redux/actionCreators/setUpGame.js
+++ src/redux/actionCreators/setUpGame.js
@@ -9,27 +9,11 @@ const url = domain + "/messages";
 //battleship-capstone-api.herokuapp.com/messages
 
 export const placeShip = messageData => dispatch => {
-  dispatch({
-    type: PLACESHIP.START
+  console.log(messageData)
+  return dispatch({
+    type: PLACESHIP.SUCCESS,
+    payload: messageData
   });
-
-  const token = JSON.parse(localStorage.login).result.token;
-
-  return fetch(url, {
-    method: "POST",
-    headers: { ...jsonHeaders, Authorization: "Bearer " + token },
-    body: JSON.stringify(messageData)
-  })
-    .then(handleJsonResponse)
-    .then(result => {
-      return dispatch({
-        type: PLACESHIP.SUCCESS,
-        payload: result
-      });
-    })
-    .catch(err => {
-      return Promise.reject(dispatch({ type: PLACESHIP.FAIL, payload: err }));
-    });
 };
 
 export const selectShip = messageData => dispatch => {
