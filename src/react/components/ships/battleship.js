a716742046b8a60c8abed7de972545d399ca2baa
Karen Marvel <knmarvel@gmail.com>
2020-01-13 12:45:04 -0500

    cursor deal in public folder

--- src/react/components/ships/battleship.js
+++ src/react/components/ships/battleship.js
@@ -25,6 +25,7 @@ class Battleship extends React.Component {
   };
 
   onShipClick = () => {
+    document.body.style.cursor = "url('/missToken.png'), help"
     this.props.selectShip({
       name: this.state.name,
       length: this.state.length,
