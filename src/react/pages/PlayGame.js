08987e41087e92639200ddb5e05eeb90662f73a8
Janell Huyck <janell.huyck@outlook.com>
2020-01-14 01:55:14 -0500

    2 am work in progress

--- src/react/pages/PlayGame.js
+++ src/react/pages/PlayGame.js
@@ -10,8 +10,41 @@ import {
   FireButton,
   TurnHandler
 } from "../components/playGame";
+import { connect } from "../HOCs";
+import board from "../components";
+import { startBoard, getOldMessages } from "../../redux/index";
 
 class PlayGame extends React.Component {
+  componentDidMount = () => {
+    this.findOpponentShipCoordinates();
+  };
+
+  findOpponentShipCoordinates = () => {
+    let opponentName = this.determineOpponent();
+    console.log(opponentName);
+    let shipCoordinates = this.props
+      .getOldMessages(opponentName)
+      .then(console.log(shipCoordinates));
+    //   .then(result => {
+    //     result.payload.map(message => {
+    //       if (!result.payload.includes("ready")) {
+    //         let newObject = {};
+    //         let splitMessage = message.text.split(" ");
+    //         let messageCoordinate = splitMessage[3];
+    //         let messageShipName = splitMessage[2];
+    //         return (newObject[messageCoordinate] = messageShipName);
+    //       }
+    //     });
+    //   });
+    // console.log(shipCoordinates);
+  };
+
+  determineOpponent = () => {
+    if (this.props.playerName === "playerA") {
+      return "playerB";
+    } else return "playerA";
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -27,9 +60,17 @@ class PlayGame extends React.Component {
     );
   }
 }
-const mapStateToProps = {
-  
-}
+const mapStateToProps = state => {
+  if (state.welcome.startGame.result) {
+    return {
+      playerName: state.welcome.startGame.result.username
+    };
+  } else return {};
+};
 
+const mapDispatchToProps = {
+  getOldMessages,
+  startBoard
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlayGame);
