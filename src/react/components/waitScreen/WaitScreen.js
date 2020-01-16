6c3d0cb079472941274e867ee75f9a1a91236c07
Karen Marvel <knmarvel@gmail.com>
2020-01-15 23:16:56 -0500

    simple credits fixed

--- src/react/components/waitScreen/WaitScreen.js
+++ src/react/components/waitScreen/WaitScreen.js
@@ -1,30 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./WaitScreen.css";
 import { deleteMessage, getOldMessages } from "../../../redux/index";
 import { connect, withAsyncAction } from "../../HOCs";
 
-
-
 class WaitScreen extends React.Component {
-  handleGoToCreditsClick = event => {
-    
-  }
-  isThisAGameEndingMessage = (trueOrFalse) =>{
-    if(trueOrFalse){
+  handleGoToCreditsClick = event => {};
+  isThisAGameEndingMessage = trueOrFalse => {
+    if (trueOrFalse) {
       return (
         <div>
-         {/* <Link to="/Credits">Credits </Link>; */}
-         Product Owner: <a href="https://gitlab.com/Janell ">Janell Hyuck</a>
-         Design & CSS Engineers: LeighAnn Featheringill & <a href="https://gitlab.com/givond4">Givon Dickerson</a>
-         Javascript, React, Redux Engineers: <a href="https://gitlab.com/And1drew">Andrew Belanger</a>, <a href="https://www.gitlab.com/chelseawhite">Chelsea White</a>, <a href="https://gitlab.com/dpdalsania">Drashti Dalsania</a>, <a href="https://gitlab.com/knmarvel">Kano Marvel</a>
-         API: <a href="https://github.com/thurt">Taylor Hurt</a>
-
-         <Link to="/">PLAY AGAIN?</Link>
-       </div>
-        )
+          {/* <Link to="/Credits">Credits </Link>; */}
+          Product Owner: Janell Hyuck; Design & CSS: LeighAnn Featheringill &
+          Givon Dickerson; Javascript, React, Redux: Andrew Belanger, Chelsea
+          White, Drashti Dalsania, Kano Marvel; API: Taylor Hurt
+          <Link
+            to="/"
+            title="Product Owner: Janell Hyuck; Design & CSS: LeighAnn Featheringill & Givon Dickerson; Javascript, React, Redux: Andrew Belanger, Chelsea White, Drashti Dalsania, Kano Marvel; API: Taylor Hurt"
+          >
+            PLAY AGAIN?
+          </Link>
+        </div>
+      );
     }
-  }
+  };
   render() {
     return (
       <div className="waitScreen">
